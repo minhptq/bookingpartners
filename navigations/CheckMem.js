4398208e96d7d20e535f83061bf18e9aa0a6338c
phamtuquocminh <minhptq@gmail.com>
2022-12-20 22:14:55 +0700

    vscode autosave

--- navigations/CheckMem.js
+++ navigations/CheckMem.js
@@ -1,12 +1,14 @@
 import { StyleSheet, Text, View } from "react-native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Signup from "../screens/Signup";
+import Signin from "../screens/Signin";
 
 const Stack = createNativeStackNavigator();
 export default function CheckMem() {
   return (
     <Stack.Navigator>
       <Stack.Screen name="Signup" component={Signup} />
+      <Stack.Screen name="Signup" component={Signin} />
     </Stack.Navigator>
   );
 }
