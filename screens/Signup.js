ff20360c60fa257270d383b03748f90afd9e72ef
phamtuquocminh <minhptq@gmail.com>
2022-12-20 22:00:08 +0700

    vscode autosave

---
+++ screens/Signup.js
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from "react-native";
+
+export default function Signup() {
+  return (
+    <View style={styles.container}>
+      <Text></Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {},
+});
