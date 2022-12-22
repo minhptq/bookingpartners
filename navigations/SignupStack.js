6be8740c4c9fcee41171a14d1e81d24df435d4b7
phamtuquocminh <minhptq@gmail.com>
2022-12-22 21:27:37 +0700

    vscode autosave

--- navigations/SignupStack.js
+++ navigations/SignupStack.js
@@ -1,11 +1,12 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import Signup from "../screens/Signup";
+import PhoneOrEmail from "../screens/PhoneOrEmail";
+
 const Stack = createNativeStackNavigator();
 
 export default function SignupStack() {
   return (
     <Stack.Navigator>
-      <Stack.Screen name="Signup" component={Signup} />
+      <Stack.Screen name="PhoneOrEmail" component={PhoneOrEmail} />
     </Stack.Navigator>
   );
 }
