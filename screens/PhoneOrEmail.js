f141736b4e94bb3fe370f68285f6fdca2e0e9c87
phamtuquocminh <minhptq@gmail.com>
2022-12-22 22:08:09 +0700

    vscode autosave

--- screens/PhoneOrEmail.js
+++ screens/PhoneOrEmail.js
@@ -1,10 +1,12 @@
 import { StyleSheet, Text, View, Modal, SafeAreaView } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
 
 export default function PhoneOrEmail() {
   return (
     <Modal>
       <View style={styles.container}>
         <SafeAreaView></SafeAreaView>
+        <Ionicons name="ios-close" size={24} color="black" />
         <Text>Phone or email </Text>
       </View>
     </Modal>
