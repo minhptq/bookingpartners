0e877becc8cd3296fa09f8409a836f5a504e25d5
phamtuquocminh <minhptq@gmail.com>
2022-12-25 23:11:33 +0700

    vscode autosave

--- screens/PhoneOrEmail.js
+++ screens/PhoneOrEmail.js
@@ -39,7 +39,7 @@ export default function PhoneOrEmail() {
 
 const styles = StyleSheet.create({
   container: {},
-  closebtn: { paddingRight: 10 },
+  closebtn: { paddingLeft: 10 },
   loginbtn: { alignSelf: "flex-end", paddingRight: 7 },
   text: { margin: 6 },
 });
