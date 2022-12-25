89c99f81aeb082d92129e44b45e2bd2165eb1ed9
phamtuquocminh <minhptq@gmail.com>
2022-12-25 22:51:15 +0700

    vscode autosave

--- screens/PhoneOrEmail.js
+++ screens/PhoneOrEmail.js
@@ -16,12 +16,16 @@ export default function PhoneOrEmail() {
         <SafeAreaView></SafeAreaView>
         <Table borderStyle={{ borderWidth: 2, borderColor: "#c8e1ff" }}>
           <Row
-            data={state.tableHead}
+            data={[
+              () => {
+                <Ionicons name="ios-close" size={24} color="black" />;
+              },
+            ]}
             style={styles.head}
             textStyle={styles.text}
           />
         </Table>
-        <Ionicons name="ios-close" size={24} color="black" />
+
         <TouchableOpacity style={styles.loginbtn}>
           <Text>Login</Text>
         </TouchableOpacity>
