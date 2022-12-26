9d8fd48708e156d655132cd568f719d7b28cf983
phamtuquocminh <minhptq@gmail.com>
2022-12-26 21:26:25 +0700

    vscode autosave

--- screens/RoomBooking.js
+++ screens/RoomBooking.js
@@ -1,14 +1,43 @@
-import { SafeAreaView, StyleSheet, Text, View } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Modal,
+  SafeAreaView,
+  TouchableOpacity,
+} from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+import { Table, Row, Rows } from "react-native-table-component";
 
 export default function RoomBooking() {
   return (
-    <View style={styles.container}>
-      <SafeAreaView></SafeAreaView>
-      <Text>Booking</Text>
-    </View>
+    <Modal>
+      <View style={styles.container}>
+        <SafeAreaView></SafeAreaView>
+        <Table borderStyle={{ borderWidth: 2, borderColor: "#c8e1ff" }}>
+          <Row
+            data={[
+              <Ionicons
+                name="ios-close"
+                size={24}
+                color="black"
+                style={styles.closebtn}
+              />,
+              <TouchableOpacity style={styles.loginbtn}>
+                <Text>Login</Text>
+              </TouchableOpacity>,
+            ]}
+          />
+        </Table>
+
+        <Text>Room Booking </Text>
+      </View>
+    </Modal>
   );
 }
 
 const styles = StyleSheet.create({
   container: {},
+  closebtn: { paddingLeft: 3 },
+  loginbtn: { alignSelf: "flex-end", paddingRight: 7 },
 });
