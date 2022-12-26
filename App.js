8a10be866b4e64bef980a849eda3f671dc5ae58d
phamtuquocminh <minhptq@gmail.com>
2022-12-26 21:35:04 +0700

    vscode autosave

--- App.js
+++ App.js
@@ -8,6 +8,7 @@ export default function App() {
   return (
     <NavigationContainer>
       <IsPartner />
+      <StatusBar style="dark" backgroundColor="white" />
     </NavigationContainer>
   );
 }
