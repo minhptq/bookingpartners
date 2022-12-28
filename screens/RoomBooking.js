800ddeac849978e15c40e68af3f62986a1871ed3
phamtuquocminh <minhptq@gmail.com>
2022-12-28 21:46:24 +0700

    vscode autosave

--- screens/RoomBooking.js
+++ screens/RoomBooking.js
@@ -28,7 +28,7 @@ export default function RoomBooking() {
               backgroundColor: "white",
               justifyContent: "center",
               padding: 4,
-              maxHeight: 20,
+              maxHeight: 23,
             }}
           >
             <Text style={{ fontSize: 18 }}>Login</Text>
