e7c86d41c2b14ede4c75814eee4cb15926eff4bc
phamtuquocminh <minhptq@gmail.com>
2023-01-04 22:31:14 +0700

    vscode autosave

--- screens/RoomBooking.js
+++ screens/RoomBooking.js
@@ -85,7 +85,7 @@ export default function RoomBooking() {
                 </TouchableOpacity>
               </View>
             }
-            stickyHeaderIndices={[0]}
+            stickyHeaderIndices={["0"]}
             stickyHeaderHiddenOnScroll={false}
             renderItem={renderItem}
             keyExtractor={(item) => item.id}
