f19c3b516c317035c604a27ab70cb82312781768
phamtuquocminh <minhptq@gmail.com>
2023-02-08 21:09:01 +0700

    vscode autosave

--- screens/RoomBooking.js
+++ screens/RoomBooking.js
@@ -129,7 +129,7 @@ const styles = StyleSheet.create({
     padding: 20,
     marginVertical: 0,
     marginHorizontal: 0,
-    width: "47%",
+    width: "49%",
     borderWidth: 1,
     height: 300,
   },
