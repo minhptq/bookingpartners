8bcea0df16a45bb043f0da8ba245286c23207590
phamtuquocminh <minhptq@gmail.com>
2023-02-08 21:50:52 +0700

    vscode autosave

--- screens/RoomBooking.js
+++ screens/RoomBooking.js
@@ -89,7 +89,7 @@ export default function RoomBooking() {
             renderItem={renderItem}
             keyExtractor={(item) => item.id}
             numColumns={2}
-            columnWrapperStyle={{ justifyContent: "space-between" }}
+            columnWrapperStyle={{ justifyContent: "space-between", width: 5 }}
             showsVerticalScrollIndicator={false}
             contentContainerStyle={{}}
             ItemSeparatorComponent={
