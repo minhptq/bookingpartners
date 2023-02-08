3d5957632477a6b68b6ee958a3a35f32fc5ac821
phamtuquocminh <minhptq@gmail.com>
2023-02-08 21:31:42 +0700

    vscode autosave

--- screens/RoomBooking.js
+++ screens/RoomBooking.js
@@ -75,6 +75,7 @@ export default function RoomBooking() {
         <View style={styles.flatlistcontainer}>
           <FlatList
             data={DATA}
+            style={{ flex: 1 }}
             StickyHeaderComponent={
               <View style={styles.header}>
                 <View style={styles.closebtn}>
