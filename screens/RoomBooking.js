f49257c028ce7eb2948934a86cc1feb61e04a366
phamtuquocminh <minhptq@gmail.com>
2023-02-09 20:54:54 +0700

    vscode autosave

--- screens/RoomBooking.js
+++ screens/RoomBooking.js
@@ -68,7 +68,7 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     flex: 1,
     margin: 2,
-    borderRadius: 5,
+    borderRadius: 7,
     height: Dimensions.get("window").width / numColumns, // approximate a square
   },
   itemInvisible: {
